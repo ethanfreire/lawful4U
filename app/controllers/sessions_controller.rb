8d20f09923f300ee4a5eadac4785486157f72173
tariqjunaid <junaidtariq_91@yahoo.com>
2019-06-19 11:00:27 -0400

    Added Authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
 
   def create
       @user = User.find_by(username: params[:username])
-    #   undefined method `authenticate' for nil:NilClass
       if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
         redirect_to clients_path
